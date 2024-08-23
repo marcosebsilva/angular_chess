5f3288c3e8c256a626c9bc9b451d4df9f83b7391
Marcos <dusdoom.dev@gmail.com>
2024-08-23 17:23:09 -0300

    feat: updated app files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,12 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { BoardComponent } from './components/board/board.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, BoardComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'frontend';
